049505329b1eebc3dfcecd027986e1125837cd86
Enol Iglesias <enoliglesias@gmail.com>
2016-07-01 14:21:34 +0200

    Add aministrate:install things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :packages
+
+    root to: "packages#index"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
